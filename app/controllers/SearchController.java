d1c4833112a1da1c334e315c195c7949001ec27b
TomastpPereira <tomas.tp.pereira@gmail.com>
2024-11-02 14:39:05 -0400

    Added query buffer functionality. TODO: - Properly display info for each video - Include thumbnail display for videos - Individual parts

--- app/controllers/SearchController.java
+++ app/controllers/SearchController.java
@@ -6,10 +6,14 @@ import play.mvc.Result;
 import com.fasterxml.jackson.databind.JsonNode;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class SearchController extends Controller {
     private final YouTube youtubeApiClient;
+    private static final int RESULTS_PER_QUERY = 10;
+    private static final int MAX_SEARCHES = 10;
+    private static final LinkedList<SearchResult> searchHistory = new LinkedList<>();
 
     @Inject
     public SearchController(YouTube youtubeApiClient) {
@@ -18,7 +22,7 @@ public class SearchController extends Controller {
 
     public Result searchVideos(String query) {
         try {
-            JsonNode videosJson = youtubeApiClient.searchVideos(query, 10);
+            JsonNode videosJson = youtubeApiClient.searchVideos(query, RESULTS_PER_QUERY);
             List<Video> videos = new ArrayList<>();
             if (videosJson != null && videosJson.get("items") != null) {
                 for (JsonNode item : videosJson.get("items")) {
@@ -32,10 +36,26 @@ public class SearchController extends Controller {
                 }
             }
 
-            return ok(views.html.index.render(query, videos));
+            if (searchHistory.size() == MAX_SEARCHES) {
+                searchHistory.removeLast();
+            }
+            searchHistory.addFirst(new SearchResult(query, videos));
+
+            //return ok(views.html.index.render(query, videos));
+            return ok(views.html.index.render(searchHistory));
         } catch (Exception e) {
             return internalServerError("Video Not Found or some other errors : " + e.getMessage());
         }
+    }
+
+    public static class SearchResult {
+        public String query;
+        public List<Video> videos;
+        public SearchResult(String query, List<Video> videos) {
+            this.query = query;
+            this.videos = videos;
+        }
+
     }
 
 }
