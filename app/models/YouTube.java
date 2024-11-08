e1412e0240e62ce22a70ca25940d2ef5cc9dc5b7
Sam Collin <95706405+GitHubSamm@users.noreply.github.com>
2024-11-08 00:33:37 -0500

    Update : Search process for latest and more Javadocs - The videos retrieved are now the latest ones. - The app handles many words queries - Javadocs for my part of the Youtube class.

--- app/models/YouTube.java
+++ app/models/YouTube.java
@@ -10,15 +10,33 @@ import java.io.InputStreamReader;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 
+/**
+ * @author Tongzhou Qian
+ * @author Sam Collin
+ * Youtube class implements method to interact with the API of Youtube.
+ * It allows to search for videos, and video details, channels and channel details.
+ * It simplifies the interaction with the API and manage the response to get it in the right format.
+ */
 public class YouTube {
+
     //private static final String API_KEY="AIzaSyBTdLng0J0bxQOYFhKhMrI23guTCVRI1xQ"; //Qian
     private static final String API_KEY="AIzaSyDO584JNmQbEi6yDkuG_UgNVVAtF4vHclU";   //Tomas
     private static final String API_URL="https://www.googleapis.com/youtube/v3/";
+
+    /**
+     * @author Tongzhou Qian
+     * @author Sam Collin
+     * Retrieves the last {@code maxResults} videos posted related to a given query.
+     * @param query Search query entered by the user. Only handles single word for now.
+     * @param maxResults The number of videos we want to retrieve.
+     * @return {@link JsonNode} having a list of videos from the channel given ordered from the most recent.
+     * @throws Exception if an error occurs.
+     */
     public JsonNode searchVideos(String query, int maxResults) throws Exception {
         // Ensures that the format of query is correct when using multiple keywords
         String encodedQuery = URLEncoder.encode(query, StandardCharsets.UTF_8);
         // Complete the url to make the request
-        String url = API_URL + "search?part=snippet&q=" + encodedQuery + "&maxResults=" + maxResults + "&key=" + API_KEY;
+        String url = API_URL + "search?part=snippet&q=" + encodedQuery + "&maxResults=" + maxResults + "&order=date&key=" + API_KEY;
         return makeRequest(url);
     }
     // Get video details by video ID
@@ -31,8 +49,17 @@ public class YouTube {
         String url = API_URL + "channels?part=snippet,statistics&id=" + channelId + "&key=" + API_KEY;
         return makeRequest(url);
     }
-    // Get video details only from the channel
+
+    /**
+     * @author Sam Collin
+     * Retrieves the last {@code maxResults} videos posted by a given {@link Channel}.
+     * @param channelId The channel where from the videos will be searched.
+     * @param maxResults The number of videos we want to retrieve.
+     * @return {@link JsonNode} having a list of videos from the channel given ordered from the most recent.
+     * @throws Exception if there is an error.
+     */
     public JsonNode getVideosByChannelId(String channelId, int maxResults) throws Exception {
+        // Get video details only from the channel
         String url = API_URL + "search?part=snippet&channelId=" + channelId + "&maxResults=" + maxResults + "&order=date&key=" + API_KEY;
         return makeRequest(url);
     }
